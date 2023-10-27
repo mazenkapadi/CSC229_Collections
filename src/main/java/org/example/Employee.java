3f6edab101b534577139b4cd99666344c310de05
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-10-27 00:53:27 -0400

    Final

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,70 @@
-package org.example;public class Employee {
+package org.example;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+public class Employee {
+    int id;
+    String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Employee(Employee other) {
+        this.id = other.id;
+        this.name = other.name;
+    }
+
+    public Employee deepCopy() {
+        return new Employee(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Employee " +
+                "id=" + id +
+                ", name='" + name + '\'';
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        return Objects.equals(name, employee.name);
+    }
+
+    public static void showAll(List<Employee> list) {
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
+    }
+
+    public static void showAllUsingIterator(List<Employee> list) {
+        Iterator<Employee> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Employee employee = iterator.next();
+            System.out.println(employee);
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,33 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.LinkedList;
+import static org.example.Employee.showAll;
+import static org.example.Employee.showAllUsingIterator;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create an instance of an ArrayList of Employee.
+        // Add 3 employees to the ArrayList.
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "John"));
+        employees.add(new Employee(2, "Jane"));
+        employees.add(new Employee(3, "Jack"));
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Call the showAll method and pass the ArrayList as a parameter.
+        showAll(employees);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        LinkedList<Employee> employees2 = new LinkedList<>();
+        employees2.add(new Employee(1, "Jake"));
+        employees2.add(new Employee(2, "Jill"));
+        employees2.add(new Employee(3, "Jen"));
+
+        showAll(employees2);
+
+        // Test showAllUsingIterator for ArrayList
+        showAllUsingIterator(employees);
+
+        // Test showAllUsingIterator for LinkedList
+        showAllUsingIterator(employees2);
     }
 }
